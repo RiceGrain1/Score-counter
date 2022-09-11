24aeda0030abd326380911f8873641062f41dd2c
RiceGrain1 <velocitysk99@gmail.com>
2022-09-11 12:48:30 -0500

    Fixed variable scope issue with addOne and addTwo functions; WIP on addThree

--- index.js
+++ index.js
@@ -4,18 +4,35 @@ let guestScore = 0
 let guestScoreEl = document.getElementById("guest-score")
 let home = "home"
 let guest = "guest"
+
 function addOne(team) {
    if (team === home) {
-    let homeValue = homeScore++
-    homeScoreEl.textContent = homeValue
-    console.log("Home team has been given a point")
+     homeScore += 1
+    homeScoreEl.textContent = homeScore
+    console.log("Home team has been given 1 point")
    }
   else if (team === guest) {
-    let guestValue = guestScore++
-    guestScoreEl.textContent = guestValue
-    console.log("Guest team has been given a point")
+     guestScore += 1
+    guestScoreEl.textContent = guestScore
+    console.log("Guest team has been given 1 point")
    }
    else {
     console.log("Something broke")
    }
 }
+
+function addTwo(team) {
+    if (team === home) {
+     homeScore += 2
+     homeScoreEl.textContent = homeScore
+     console.log("Home team has been given 2 points")
+    }
+   else if (team === guest) {
+      guestScore += 2
+     guestScoreEl.textContent = guestScore
+     console.log("Guest team has been given 2 point")
+    }
+    else {
+     console.log("Something broke")
+    }
+ }
